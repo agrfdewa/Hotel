74758ad97c9c605cb4993ac088bbe72b097a33e0
noviarn <42713448+noviarn@users.noreply.github.com>
2020-06-02 04:14:43 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <div class="col-md-6">
         <div class="card mx-4">
             <div class="card-body p-4">
-            <h1 style="text-align: center;">Log in to {{ trans('panel.site_title') }}</h1>
+                <h1 style="text-align: center;">Log in to {{ trans('panel.site_title') }}</h1>
 
                 <br>
 
